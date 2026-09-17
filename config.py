89dc6c3d41bc59aaf34da6a125af785ea3de6f50
ashyamctommy-eng <ashyamctommy@gmail.com>
2026-09-17 15:59:28 +0300

    Update config.py
    #UPDATE BOT TOKEN

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Pyrogram setup
 API_ID = "38807471"  # Replace this API ID with your actual API ID
 API_HASH = "9bbfb9efe1a47596cf7f1b20017f5dc6"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "8783734837:AAHRc26SjGkCJufDghCk-08xuErxtJ6bIbQ"  # Replace this BOT_TOKEN
+BOT_TOKEN = "8660856028:AAG53lX1VbyfCLaWiDctQMbRv1MZPwgarz4"  # Replace this BOT_TOKEN
 
 # Admin IDs
-ADMIN_IDS = [5248903529,8650292769,908723940]
+ADMIN_IDS = [5541778617,8650292769,908723940]
 
 
 DEFAULT_LIMIT = 10000
