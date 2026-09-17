3bc73d6b575358d3cb3cae0fbd55a61852611b00
ashyamctommy-eng <ashyamctommy@gmail.com>
2026-09-17 15:38:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Pyrogram setup
+API_ID = "38807471"  # Replace this API ID with your actual API ID
+API_HASH = "9bbfb9efe1a47596cf7f1b20017f5dc6"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8783734837:AAHRc26SjGkCJufDghCk-08xuErxtJ6bIbQ"  # Replace this BOT_TOKEN
+
+# Admin IDs
+ADMIN_IDS = [5248903529,8650292769,908723940]
+
+
+DEFAULT_LIMIT = 10000
+ADMIN_LIMIT = 50000
